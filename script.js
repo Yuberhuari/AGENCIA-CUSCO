bfcf5a0ae177f7063607725fbf4ab1d44922a97f
Yuberhuari <200962@unsaac.edu.pe>
2026-01-06 14:56:45 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// Scroll suave
+document.querySelectorAll('a[href^="#"]').forEach(anchor=>{
+  anchor.addEventListener('click',e=>{
+    e.preventDefault();
+    document.querySelector(anchor.getAttribute('href'))
+      .scrollIntoView({behavior:'smooth'});
+  });
+});
